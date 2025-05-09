87778158bb72d8bfcde11edde94ace78b22fdd07
Phil Davies <git@karn.co.uk>
2025-05-09 20:30:28 +0100

    wip: ktor-raise-server with context-parameters

--- arrow-libs/integrations/arrow-raise-ktor-server/src/commonMain/kotlin/arrow/raise/ktor/server/raiseError.kt
+++ arrow-libs/integrations/arrow-raise-ktor-server/src/commonMain/kotlin/arrow/raise/ktor/server/raiseError.kt
@@ -4,9 +4,12 @@ import arrow.core.NonEmptyList
 import arrow.core.raise.Raise
 import arrow.core.raise.withError
 import arrow.raise.ktor.server.request.RequestError
+import io.ktor.server.routing.RoutingContext
 
+context(r: Raise<Response>)
 @PublishedApi
-internal inline fun <R> RaiseRoutingContext.raiseError(f: Raise<RequestError>.() -> R): R = withError(call::errorResponse, f)
+internal inline fun <R> RoutingContext.raiseError(f: Raise<RequestError>.() -> R): R = r.withError(call::errorResponse, f)
 
+context(r: Raise<Response>)
 @PublishedApi
-internal inline fun <R> RaiseRoutingContext.raiseErrors(f: Raise<NonEmptyList<RequestError>>.() -> R): R = withError(call::errorsResponse, f)
+internal inline fun <R> RoutingContext.raiseErrors(f: Raise<NonEmptyList<RequestError>>.() -> R): R = r.withError(call::errorsResponse, f)
--- arrow-libs/integrations/arrow-raise-ktor-server/src/commonMain/kotlin/arrow/raise/ktor/server/respondOrRaise.kt
+++ arrow-libs/integrations/arrow-raise-ktor-server/src/commonMain/kotlin/arrow/raise/ktor/server/respondOrRaise.kt
@@ -1,5 +1,6 @@
 package arrow.raise.ktor.server
 
+import arrow.core.raise.Raise
 import arrow.raise.ktor.server.request.receiveNullableOrRaise
 import io.ktor.http.*
 import io.ktor.server.http.content.*
@@ -7,13 +8,14 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.util.reflect.*
 
-public typealias RespondingRaiseRoutingHandler<TResponse> = suspend RaiseRoutingContext.() -> TResponse
+public typealias RespondingRaiseRoutingHandler<TResponse> = suspend context(Raise<Response>) RoutingContext.() -> TResponse
 
 // due to compilation ambiguity between n-ary lambdas on function resolution, by using this SAM on the API it's resolved with a lower priority
 // which mitigates the ambiguity of `handler { }` vs `handler { it -> }`
 // equivalent of `suspend context(Raise<Response>) RoutingContext.(TRequest) -> TResponse`
 public fun interface ReceivingRespondingRaiseRoutingHandler<TRequest, TResponse>{
-  public suspend fun RaiseRoutingContext.handle(request: TRequest): TResponse
+  context(_: Raise<Response>)
+  public suspend fun RoutingContext.handle(request: TRequest): TResponse
 }
 
 public suspend inline fun <reified TResponse> RoutingContext.respondOrRaise(
@@ -46,7 +48,7 @@ internal suspend inline fun <TRequest, TResponse> RoutingContext.respondOrRaise(
 ): Unit = handleOrRaise {
   val request: TRequest = raiseError {
     @Suppress("UNCHECKED_CAST") // TODO: if TRequest is nullable do we break things with this?
-    receiveNullableOrRaise<Any>(call, requestTypeInfo) as TRequest
+    call.receiveNullableOrRaise<Any>(requestTypeInfo) as TRequest
   }
   val result = with(body) { handle(request) }
   call.respondSafely(statusCode, result, responseTypeInfo)
